1651c0280512e5a03860392687964fc72bb63a01
Marco Parra <marcoparra311@gmail.com>
2024-08-21 12:55:37 -0400

    Create section-1 (heading)

--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -1,32 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, OnInit, output, signal } from '@angular/core';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  output,
+} from '@angular/core';
 @Component({
   selector: 'heading',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
-
   darkMode: boolean = false;
   onSetDarkMode = output<boolean>();
 
   ngOnInit(): void {
     if (localStorage.getItem('darkTheme') === 'true') {
-      this.onSetDarkMode.emit(this.darkMode = true);
+      this.onSetDarkMode.emit((this.darkMode = true));
       return;
     }
-    this.onSetDarkMode.emit(this.darkMode = false);
+    this.onSetDarkMode.emit((this.darkMode = false));
   }
 
-  setDarkMode():void {
-    this.onSetDarkMode.emit(this.darkMode = !this.darkMode);
+  setDarkMode(): void {
+    this.onSetDarkMode.emit((this.darkMode = !this.darkMode));
     localStorage.setItem('darkTheme', JSON.stringify(this.darkMode));
   }
-
 }
