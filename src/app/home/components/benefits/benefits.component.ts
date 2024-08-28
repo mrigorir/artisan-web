d7440285a3358ecfb78ec0780f079e9b3870ea33
Marco Parra <marcoparra311@gmail.com>
2024-08-27 21:16:07 -0400

    update section 5

--- src/app/home/components/benefits/benefits.component.ts
+++ src/app/home/components/benefits/benefits.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { withDebugTracing } from '@angular/router';
 
 @Component({
   selector: 'benefits',
