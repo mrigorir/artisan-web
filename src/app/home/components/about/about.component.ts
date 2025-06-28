d2fa1143de7c4c37c2b0970c55ffc40559072d57
Marco Parra <marcoparra311@gmail.com>
2025-06-28 19:33:24 -0400

    Clean header and footer dummy examples styles, now ready to be used with respective structure

--- src/app/home/components/about/about.component.ts
+++ src/app/home/components/about/about.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { HeaderComponent } from '../../../shared/header/header.component';
+import { FooterComponent } from '../../../shared/footer/footer.component';
+
 @Component({
   selector: 'about',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutComponent { }
+export class AboutComponent {}
